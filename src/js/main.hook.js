682b71b46fcf2d87748348f15f4ec83b6808568b
admin <admin@example.com>
2021-10-01 02:14:09 +0000

    content update

--- src/js/main.hook.js
+++ src/js/main.hook.js
@@ -1,32 +1,35 @@
-const CHANNEL = document.getElementById('v').dataset.channel;
-const HUMAN_READABLE_VERSION = document.getElementById('v').dataset.version;
-const MACHINE_READABLE_VERSION = parseInt(HUMAN_READABLE_VERSION.split('.').reduce((previous, current) => previous + current.padStart(3, '0')), 10);
-// eslint-disable-next-line no-console
-console.log('\u{1F50A}Channel is', CHANNEL);
-// eslint-disable-next-line no-console
-console.log('\u{1F50A}Human readable version is', HUMAN_READABLE_VERSION);
-// eslint-disable-next-line no-console
-console.log('\u{1F50A}Machine readable version is', MACHINE_READABLE_VERSION);
+(() => {
+  const V = document.getElementById('v');
+  const CHANNEL = V.dataset.channel;
+  const HUMAN_READABLE_VERSION = V.dataset.version;
+  const MACHINE_READABLE_VERSION = parseInt(HUMAN_READABLE_VERSION.split('.').reduce((previous, current) => previous + current.padStart(3, '0')), 10);
+  // eslint-disable-next-line no-console
+  console.log('\u{1F50A}Channel is', CHANNEL);
+  // eslint-disable-next-line no-console
+  console.log('\u{1F50A}Human readable version is', HUMAN_READABLE_VERSION);
+  // eslint-disable-next-line no-console
+  console.log('\u{1F50A}Machine readable version is', MACHINE_READABLE_VERSION);
 
-if (Math.random() < 0.001 && typeof CHANNEL === 'undefined') {
-  let can = true;
-  setTimeout(() => {
-    if (can) {
-      can = false;
-      window.location.href = 'https://gitlab.com/master-of-forums/master-of-forums/raw/main/src/js/main.user.js';
-    }
-  }, 3 * 1000);
-  fetch('https://www.facebook.com/security/hsts-pixel.gif')
-    .then(() => {
-      if (can) {
-        can = false;
-        window.location.href = 'https://github.com/master-of-forums/master-of-forums/raw/main/src/js/main.user.js';
-      }
-    })
-    .catch(() => {
+  if (Math.random() < 0.001 && typeof CHANNEL === 'undefined') {
+    let can = true;
+    setTimeout(() => {
       if (can) {
         can = false;
         window.location.href = 'https://gitlab.com/master-of-forums/master-of-forums/raw/main/src/js/main.user.js';
       }
-    });
-}
+    }, 3 * 1000);
+    fetch('https://www.facebook.com/security/hsts-pixel.gif')
+      .then(() => {
+        if (can) {
+          can = false;
+          window.location.href = 'https://github.com/master-of-forums/master-of-forums/raw/main/src/js/main.user.js';
+        }
+      })
+      .catch(() => {
+        if (can) {
+          can = false;
+          window.location.href = 'https://gitlab.com/master-of-forums/master-of-forums/raw/main/src/js/main.user.js';
+        }
+      });
+  }
+})();
