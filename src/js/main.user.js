cd86cac4b7f7d954ac445d88753b75de30960d11
admin <admin@example.com>
2021-10-05 14:17:14 +0000

    content update

--- src/js/main.user.js
+++ src/js/main.user.js
@@ -1190,10 +1190,10 @@ const MASTER_OF_FORUMS = () => {
           } else if (content.includes('\u{60A8}\u{4E0D}\u{80FD}\u{5BF9}\u{81EA}\u{5DF1}\u{7684}\u{56DE}\u{5E16}')) {
             // 您不能对自己的回帖进行投票
             // noticeMessage();
-            MAIN.actions?.thumbs(POST_ID);
+            // MAIN.actions?.thumbs(POST_ID);
           } else if (content.includes('\u{60A8}\u{5DF2}\u{7ECF}\u{5BF9}\u{6B64}\u{56DE}\u{5E16}')) {
             // 您已经对此回帖投过票了
-            noticeMessage();
+            // noticeMessage();
             // MAIN.actions?.thumbs(POST_ID);
           } else if (content.includes('\u{672A}\u{5B9A}\u{4E49}\u{64CD}\u{4F5C}')) {
             // 未定义操作
